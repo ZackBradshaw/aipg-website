0f231898db197ad4161e7a6fbe0c0b5825229d6e
Zack <zack@zackbradshaw.com>
2024-06-01 00:38:21 -0500

    📝 Update BlogsCard component

--- components/BlogsCard.js
+++ components/BlogsCard.js
@@ -38,8 +38,8 @@ const BlogsCard = ({ image, title, _createdAt, overview, slug }) => {
               <path
                 d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
+                strokeWidth="2"
+                strokeLinecap="round"
               />
             </svg>
           </p>
